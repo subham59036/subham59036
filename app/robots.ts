45ab69795717099c82608c10ac108d99893c3b93
Subham Saha <subham59036@outlook.com>
2026-04-01 23:39:16 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://subhamsaha.dev/sitemap.xml',
+    sitemap: 'https://subham59036.vercel.app/sitemap.xml',
   };
 }
