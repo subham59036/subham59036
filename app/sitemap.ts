cdde9a0e6d06fc5fa46913992a0efefcf9964d92
Subham Saha <subham59036@outlook.com>
2026-04-01 23:40:01 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://subhamsaha.dev',
+      url: 'https://subham59035.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
